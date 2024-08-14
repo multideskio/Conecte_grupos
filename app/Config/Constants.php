c2055d2d01689b8430d87e3dc0c9e967dd58a11e
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-14 03:24:14 -0300

    Update Constants.php

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,3 +92,6 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
+
+// Adicione isso em Config/Constants.php ou em algum lugar global
+defined('CI_DEBUG') || define('CI_DEBUG', false);
