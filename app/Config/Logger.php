58a74c526f3226e62f635271b7443d408e2a4aa5
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-14 03:29:59 -0300

    Update Logger.php

--- app/Config/Logger.php
+++ app/Config/Logger.php
@@ -39,7 +39,7 @@ class Logger extends BaseConfig
      * @var array|int
      */
     //public $threshold = (ENVIRONMENT === 'production') ? 4 : 9;
-    public $threshold = 4;
+    public $threshold = 0;
 
     /**
      * --------------------------------------------------------------------------
